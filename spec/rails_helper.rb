65300a54ad17bba95d518e4f0110b24a865ed95f
SugiuraShota <s.shota.710.3506@gmail.com>
2017-07-17 01:10:52 +0900

    fix rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'capybara/rails'
+require 'capybara/rspec'
+
 require 'shoulda/matchers'
 require 'factory_girl_rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -22,7 +26,7 @@ require 'factory_girl_rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
